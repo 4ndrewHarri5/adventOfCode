1f0a3dc5788fd1f9e529e2bdc7f50eead6dbe2c5
Andrew Harris <4ndrew.harri5@gmail.com>
2019-12-11 10:53:27 +0000

    Added tests for rule type

--- src/test/java/AdventOfCodeTests/day2Tests/OpcodeTests.java
+++ src/test/java/AdventOfCodeTests/day2Tests/OpcodeTests.java
@@ -1,18 +1,51 @@
 package AdventOfCodeTests.day2Tests;
 
 import AdventOfCode.day2.Opcode;
+import AdventOfCode.day2.rule.Rule;
+import AdventOfCode.day2.rule.Rules;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class OpcodeTests {
 
+    @Test
+    public void whenOpcodeIs1RuleIsAdd() {
+        //Arrange
+        int opcode = 1;
+        //Act
+        Rule rule = Opcode.set(opcode).applyRule();
+        //Assert
+        Assertions.assertThat(rule).isEqualTo(Rules.ADD);
+    }
 
     @Test
-    public void whenOpcodeIs1Add2And3AndPrintResult() {
+    public void whenOpcodeIs2RuleIsMultiply() {
+        //Arrange
+        int opcode = 2;
+        //Act
+        Rule rule = Opcode.set(opcode).applyRule();
+        //Assert
+        Assertions.assertThat(rule).isEqualTo(Rules.MULTIPLY);
+    }
 
-        int opcode = 1;
-        int a = 2;
-        int b = 3;
+    @Test
+    public void whenOpcodeIs99RuleIsTerminate() {
+        //Arrange
+        int opcode = 99;
+        //Act
+        Rule rule = Opcode.set(opcode).applyRule();
+        //Assert
+        Assertions.assertThat(rule).isEqualTo(Rules.TERMINATE);
+    }
 
+    @Test
+    public void whenOpcodeIs87RuleIsInvalid() {
+        //Arrange
+        int opcode = 87;
+        //Act
+        Rule rule = Opcode.set(opcode).applyRule();
+        //Assert
+        Assertions.assertThat(rule).isEqualTo(Rules.INVALID);
     }
 
 }
